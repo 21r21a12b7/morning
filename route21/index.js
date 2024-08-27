8aa09a7c225968f4b476694feaef0fd78f66b195
21r21a12b7 <21r21a12b7@mlrinstitutions.ac.in>
2024-08-27 12:29:32 +0530

    first commit

--- route21/index.js
+++ route21/index.js
@@ -169,7 +169,8 @@ async function fetchBusLocation() {
             } else {
               userLocationMarker = L.marker([latitude, longitude], { icon: userLocationIcon }).addTo(map);
             }
-            userLocationMarker.bindPopup(`[${latitude},${longitude}]`).openPopup();
+            // userLocationMarker.bindPopup(`[${latitude},${longitude}]`).openPopup();
+            userLocationMarker.bindPopup("<b>It's You</b>").openPopup();
           },
           (error) => {
             switch (error.code) {
